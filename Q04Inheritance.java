85e2f7d80289bdee480d98f55311b0ad9ebe2d6e
Ankit Kumar <2041011129.ankitkumar@gmail.com>
2024-03-05 17:48:49 +0530

    muntiple-Inheritance

--- Q04Inheritance.java
+++ Q04Inheritance.java
@@ -1,5 +1,5 @@
 // (04/03/2024, 17:30)
-// QUES: IMPLEMENTS SINGLE, HIRARICAL AND MUNTILEVEL INHERITANCE.
+// QUES: IMPLEMENTS SINGLE, HIERARCHICAL AND MUNTILEVEL INHERITANCE. 
 package oops;
 
 // class 1
@@ -19,7 +19,7 @@ class Person {
 
 }
 
-// class 2
+// class 2 // single Inheritance (Teacher -> Person)
 class Teacher extends Person {
     String name;
     double salary;
@@ -36,7 +36,7 @@ class Teacher extends Person {
     }
 }
 
-// class 3
+// class 3 // Multilevel inheritence (MathTeacher -> Teacher -> Person)
 class MathTeacher extends Teacher {
 
     void details() {
@@ -50,7 +50,26 @@ class MathTeacher extends Teacher {
     }
 }
 
-public class Q04Inheritence {
+// class // Hierarchical Inheritance (Engineer and Teacher both are extending
+// Person class)
+class Engineer extends Person {
+    String branch;
+
+    void details() {
+        System.out.println("Name :" + this.name);
+        System.out.println("Salary : " + this.salary);
+        System.out.println("Branch : " + this.branch);
+    }
+
+    Engineer(String name, double salary, String branch) {
+        this.name = name;
+        this.salary = salary;
+        this.branch = branch;
+        System.out.println("ENGINEER CONSTRUCTOR");
+    }
+}
+
+public class Q04Inheritance {
     public static void main(String[] args) {
         Person p1 = new Person();
         p1.name = "Shiv";
@@ -63,11 +82,14 @@ public class Q04Inheritence {
         t.id = 109;
         t.details();
 
-        MathTeacher mt = new MathTeacher();
+        Teacher mt = new MathTeacher();
         t.name = "Ram";
         t.salary = 1000000.867;
         t.id = 110;
         t.details();
+
+        Engineer e = new Engineer("Ankit Kumar", 2000000, "CSIT");
+        e.details();
 
     }
 }
---
+++ Q05Inheritance.java
@@ -0,0 +1,42 @@
+// (05/03/2024, 16:55)
+// QUES: WRITE CODE FOR MUNTIPLE INHERITANCE IN JAVA.
+package oops;
+
+// Interface 1
+interface Wheel {
+    int number = 2;
+
+    void wheelDetails();
+}
+
+// Interface 2
+interface Wings {
+    String diameter = "90cm";
+
+    void wingsDetails();
+}
+
+// class // Muntiple inhertance (Helicopter class extends both Wheel and Wings Intrface)
+class Helicopter implements Wheel, Wings {
+    String diameter = "90cm";
+
+    public void wheelDetails() {
+        System.out.println("FROM WHEEL INTERFACE");
+        System.out.println("Number : " + Wheel.number);
+    }
+
+    public void wingsDetails() {
+        System.out.println("FROM WINGS INTERFACE");
+        System.out.println("Diameter : " + this.diameter);
+    }
+}
+
+public class Q05Inheritance {
+    public static void main(String[] args) {
+
+        Helicopter h = new Helicopter();
+        h.wheelDetails();
+        h.wingsDetails();
+    }
+
+}
