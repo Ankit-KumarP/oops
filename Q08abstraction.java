38185fdfbfc42ea3adf99f452bf3ff7b4b0eb2fc
Ankit Kumar <2041011129.ankitkumar@gmail.com>
2024-03-07 11:24:02 +0530

    interface

---
+++ Q08abstraction.java
@@ -0,0 +1,43 @@
+// (07/03/2024, 10:45)
+// QUES: SHOW ABSTRACTIOIN IN OOPS BY USING INTERFACE.
+package oops;
+
+// interface
+interface Eating {
+    int kg = 32;
+
+    static void special() {
+        System.out.println("I am special methode with body bro.");
+    }
+
+    void eating();
+}
+
+// interface
+interface Running {
+    double km = 45.7;
+
+    void distance();
+}
+
+// class
+class Shyam implements Eating, Running {
+
+    public void eating() {
+        System.out.println("Shyam can eat " + Eating.kg + " KG rice.");
+    }
+
+    public void distance() {
+        System.out.println("After Eating " + Eating.kg + " KG rice, Shyam can run " + Running.km + " KM.");
+    }
+
+}
+
+public class Q08abstraction {
+    public static void main(String[] args) {
+        Shyam sm = new Shyam();
+        sm.eating();
+        sm.distance();
+        Eating.special();
+    }
+}
